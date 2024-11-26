aa7685ecd1fdfc22fba0f4f33926975be954e077
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-11-26 16:09:28 -0300

    refactor(dao): Refatorada a lógica do findById, criando os métodos instantiateDepartment e instantiateSeller para melhorar o código

--- src/application/App.java
+++ src/application/App.java
@@ -2,17 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerDaoJDBC;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class App {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller seller = sellerDao.findById(3);
+        System.out.println("=== Test 1: seller findById() ===");
         System.out.println(seller);
     }
 }
