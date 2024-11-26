4012bd0041596c22fc9e2b03f74e54de4dc8b3e3
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-11-26 15:33:32 -0300

    feat(dao): Implementado 'findById' na classe SellerDaoJDBC

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,7 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,11 +11,8 @@ import java.util.Date;
 public class App {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
