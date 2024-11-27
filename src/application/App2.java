00f75763339244be42a44ea4502a9162e40b6ca4
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-11-27 18:59:40 -0300

    feat(dao): Implementada a operação 'findById' na classe DepartmentDaoJDBC

--- src/application/App2.java
+++ src/application/App2.java
@@ -7,12 +7,16 @@ import model.entities.Department;
 public class App2 {
     public static void main(String[] args) {
 
-        //Insert operation test
+        //Insert operation test for table department
         System.out.println("=== Test 1: Department insert() ===");
-        DepartmentDao dep = DaoFactory.createDepartmentDao();
-        Department newDepartment = new Department(null, "Games");
-        dep.insert(newDepartment);
-        System.out.println("Inserted! New id = " + newDepartment.getId());
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        Department department = new Department(null, "Food");
+        departmentDao.insert(department);
+        System.out.println("Inserted! New id = " + department.getId());
 
+        //findById operation test for table department
+        System.out.println("\n=== Test 2: Department findById() ===");
+        department = departmentDao.findById(5);
+        System.out.println(department);
     }
 }
